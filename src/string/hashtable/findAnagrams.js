3db602e187d658cd8055948b2c10212484b22344
Sagar Sangani <sagarsangani29@gmail.com>
2024-03-14 19:15:11 -0400

    Document findAnagrams

--- src/string/hashtable/findAnagrams.js
+++ src/string/hashtable/findAnagrams.js
@@ -32,28 +32,35 @@
  * @return {number[]}
  */
 const findAnagrams = (s, p) => {
+  const res = [];
+  // Initialize map with counters using each char in p
   const map = {};
   for (const c of p) {
     if (!map[c]) map[c] = 0;
     map[c]++;
   }
-
+  // Let counter track number of chars needed to form p
   const n = s.length;
   const m = p.length;
   let counter = m;
-  const res = [];
   for (let i = 0; i < n; i++) {
+    // Decrement char counter for newly added character
     const c = s[i];
     if (!map[c]) map[c] = 0;
+    // If char was part of p then  decrement global counter
     if (map[c] > 0) counter--;
     map[c]--;
-
+    // Since anagram has fixed length of m, the first
+    // char needs to be dropped out of window
+    // by incrementing it's char counter
     if (i >= m) {
       const sc = s[i - m];
       map[sc]++;
+      // If dropped char was needed for p then increment global counter
       if (map[sc] > 0) counter++;
     }
-
+    // If all characters in p are present then add
+    // index of beginning of substring
     if (counter == 0) {
       res.push(i - m + 1);
     }
