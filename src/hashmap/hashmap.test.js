48f69bb95bb87ac2154e26f5f89d36c4cab21b38
Sagar Sangani <sagarsangani29@gmail.com>
2024-03-19 16:10:56 -0400

    RandomizedSet

--- src/hashmap/hashmap.test.js
+++ src/hashmap/hashmap.test.js
@@ -1,7 +1,8 @@
 /* eslint no-undef: "off"*/
 const ransomNote = require('./ransomNote');
+const RandomizedSet = require('./randomizedSet');
 
-describe([
+describe.each([
   {
     note: 'a',
     magazine: 'b',
@@ -22,4 +23,15 @@ describe([
     const actual = ransomNote.canConstruct(note, magazine);
     expect(actual).toBe(expected);
   });
+});
+
+test('RandomizedSet', () => {
+  const set = new RandomizedSet();
+  expect(set.insert(1)).toBe(true);
+  expect(set.remove(2)).toBe(false);
+  expect(set.insert(2)).toBe(true);
+  set.getRandom();
+  expect(set.remove(1)).toBe(true);
+  expect(set.insert(2)).toBe(false);
+  expect(set.getRandom()).toBe(2);
 });
