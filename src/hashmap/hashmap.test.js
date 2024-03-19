21998c4908fb24185153dd6261428c61a383e1de
Sagar Sangani <sagarsangani29@gmail.com>
2024-03-19 16:54:49 -0400

    LRU Cache Naive Impl

--- src/hashmap/hashmap.test.js
+++ src/hashmap/hashmap.test.js
@@ -1,6 +1,7 @@
 /* eslint no-undef: "off"*/
 const ransomNote = require('./ransomNote');
 const RandomizedSet = require('./randomizedSet');
+const LRUCache = require('./lruCache');
 
 describe.each([
   {
@@ -25,7 +26,7 @@ describe.each([
   });
 });
 
-test('RandomizedSet', () => {
+test('Randomized Set', () => {
   const set = new RandomizedSet();
   expect(set.insert(1)).toBe(true);
   expect(set.remove(2)).toBe(false);
@@ -34,4 +35,17 @@ test('RandomizedSet', () => {
   expect(set.remove(1)).toBe(true);
   expect(set.insert(2)).toBe(false);
   expect(set.getRandom()).toBe(2);
+});
+
+test('LRU Cache', () => {
+  const lruCache = new LRUCache(2);
+  lruCache.put(1, 1);
+  lruCache.put(2, 2);
+  expect(lruCache.get(1)).toBe(1);
+  lruCache.put(3, 3);
+  expect(lruCache.get(2)).toBe(-1);
+  lruCache.put(4, 4);
+  expect(lruCache.get(1)).toBe(-1);
+  expect(lruCache.get(3)).toBe(3);
+  expect(lruCache.get(4)).toBe(4);
 });
